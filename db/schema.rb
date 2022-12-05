6d62ddfa04a2063271e8d9894108f7f5d8a448f7
Kazungu Derrick <kazunguderriq@gmail.com>
2022-12-05 07:59:32 +0300

    create migration to add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_05_042621) do
+ActiveRecord::Schema.define(version: 2022_12_05_045601) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_12_05_042621) do
     t.boolean "is_completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "account_id"
   end
 
 end
