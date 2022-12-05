465a34f74325050fafb1d45551030f18a71ab234
Kazungu Derrick <kazunguderriq@gmail.com>
2022-12-05 07:52:31 +0300

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :todo_items
   devise_for :accounts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'todo_items#index'
   
 end
--- db/migrate/20221205042621_create_todo_items.rb
+++ db/migrate/20221205042621_create_todo_items.rb
@@ -2,6 +2,7 @@ class CreateTodoItems < ActiveRecord::Migration[6.1]
   def change
     create_table :todo_items do |t|
       t.string :title
+      t.boolean :is_completed
 
       t.timestamps
     end
